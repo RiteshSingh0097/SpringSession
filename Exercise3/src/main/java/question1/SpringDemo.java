c1795baba6515d21d06f51ffd5cdde4ecb81f454
RiteshSingh0097 <ritesh893@outlook.com>
2019-03-15 11:25:08 +0530

    update

--- Exercise3/src/main/java/question1/SpringDemo.java
+++ Exercise3/src/main/java/question1/SpringDemo.java
@@ -1,4 +1,22 @@
 package question1;
 
+import java.sql.*;
+
 public class SpringDemo {
+
+    public static void main(String[] args) throws ClassNotFoundException, SQLException, IllegalAccessException, InstantiationException {
+
+        String driverName = "com.mysql.jdbc.Driver";
+        Class.forName(driverName).newInstance();
+        String connectionString = "jdbc:mysql://localhost:3306/springDemo";
+        String username = "root";
+        String password = "password";
+        Connection connection = DriverManager.getConnection(connectionString, username, password);
+        PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM user");
+        ResultSet resultSet = preparedStatement.executeQuery();
+        while (resultSet.next()) {
+            System.out.println(resultSet.getString("name"));
+        }
+
+    }
 }
